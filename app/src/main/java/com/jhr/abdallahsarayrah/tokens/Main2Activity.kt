84ec76b835b3e69f80e8deb02c70878e57c84eb1
abdallah sarayrah <abdallah.sarayrah@JPGOV.GOVER.Local>
2018-01-08 12:45:32 +0200

    Initial commit

---
+++ app/src/main/java/com/jhr/abdallahsarayrah/tokens/Main2Activity.kt
@@ -0,0 +1,40 @@
+package com.jhr.abdallahsarayrah.tokens
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+class Main2Activity : AppCompatActivity() {
+    @SuppressLint("MissingPermission")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        var manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        var s = object : LocationListener {
+            override fun onLocationChanged(p0: Location) {
+                Toast.makeText(this@Main2Activity,
+                        p0.latitude.toString() + " " + p0.longitude,
+                        Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+            }
+
+            override fun onProviderEnabled(p0: String?) {
+            }
+
+            override fun onProviderDisabled(p0: String?) {
+
+            }
+        }
+
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0.0F, s)
+    }
+}
